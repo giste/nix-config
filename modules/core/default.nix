f75767fe4db9d62a97fe58493257329f3fb2aefb
Giste <giste.trappiste@gmail.com>
2024-09-25 19:06:41 +0200

    Remove common module and stateVersion option

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -1,6 +1,48 @@
-{ ... }:
+{ config, lib, ... }:
 
 {
+  options = {
+    host = with lib; {
+      name = mkOption {
+        type = types.str;
+        description = "Name of the host";
+      };
+
+      type = mkOption {
+        type = types.enum [ "server" "desktop" ];
+        description = "Host type: server | desktop";
+        default = "server";
+      };
+
+      users = mkOption {
+        description = "Users to create in the host.";
+        type = types.listOf (types.submodule {
+          options = {
+            login = mkOption {
+              type = types.str;
+              description = "Login name of the user.";
+            };
+            name = mkOption {
+              type = types.str;
+              description = "User name to show.";
+              default = "";
+            };
+            uid = mkOption {
+              type = types.nullOr types.int;
+              description = "User UID";
+              default = null;
+            };
+            extraGroups = mkOption {
+              type = types.listOf types.str;
+              description = "Additional groups of this user.";
+              default = [];
+            };
+          };
+        });
+      };
+    };
+  };
+
   imports = [
     ./bootloader.nix
     ./timezone.nix
