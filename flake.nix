53dca30b9cb7b578f0ef2cb92375761a52a40a35
Giste <giste.trappiste@gmail.com>
2024-09-30 09:45:52 +0200

    Make clear if host is using stable or unstable channels

--- flake.nix
+++ flake.nix
@@ -4,13 +4,13 @@
 
   inputs = {
     ###### Nix packages ######
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "nixpkgs/nixos-24.05";
 
     ###### Home Manager ######
-    home-manager = {
+    home-manager-unstable = {
       url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
     home-manager-stable = {
@@ -22,7 +22,7 @@
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs = {
-        nixpkgs.follows = "nixpkgs";
+        nixpkgs.follows = "nixpkgs-unstable";
         nixpkgs-stable.follows = "nixpkgs-stable";
       };
     };
@@ -34,7 +34,7 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-stable, home-manager, home-manager-stable, sops-nix, ... }:
+  outputs = inputs@{ self, nixpkgs-unstable, nixpkgs-stable, home-manager-unstable, home-manager-stable, sops-nix, ... }:
   let
     # ------ System settings ------ 
     systemSettings = {
@@ -64,22 +64,21 @@
 
       nixos = let
         system = "x86_64-linux";
-        # Set nixpkgs-channel to nixpkgs or nixpkgs-stable
-        nixpackages = nixpkgs-stable;
-        pkgs = import nixpackages {
+        # Set channel for packages and home-manager
+        nixPackages = nixpkgs-stable;
+        homeManager = home-manager-stable;
+        pkgs = import nixPackages {
           inherit system;
           config.allowUnfree = true;
         };
-        lib = pkgs.lib;
-      in nixpackages.lib.nixosSystem {
+        inherit (nixPackages) lib;
+      in lib.nixosSystem {
         inherit system pkgs lib;
         modules = [ 
           ./hosts/idefix
-          home-manager-stable.nixosModules.home-manager {
+          homeManager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-
-            #home-manager.users.nacho = import ./user/home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
             home-manager.extraSpecialArgs = {
@@ -97,22 +96,22 @@
 
     };
 
-    homeConfigurations = {
-      nacho = home-manager-stable.lib.homeManagerConfiguration {
-        #inherit pkgs;
-        pkgs = import inputs.nixpkgs-stable {
-          system = systemSettings.system;
-          config = {
-            allowUnfree = true;
-          };
-        };
-        modules = [ ./user/home.nix ];
-        extraSpecialArgs = {
-          inherit systemSettings;
-          inherit userSettings;
-        };
-      };
-    };
+  #   homeConfigurations = {
+  #     nacho = home-manager-stable.lib.homeManagerConfiguration {
+  #       #inherit pkgs;
+  #       pkgs = import inputs.nixpkgs-stable {
+  #         system = systemSettings.system;
+  #         config = {
+  #           allowUnfree = true;
+  #         };
+  #       };
+  #       modules = [ ./user/home.nix ];
+  #       extraSpecialArgs = {
+  #         inherit systemSettings;
+  #         inherit userSettings;
+  #       };
+  #     };
+  #   };
   };
 
 }
