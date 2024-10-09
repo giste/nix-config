d68f1de1c33264210ca0428baf920e36f852c6bc
Giste <giste.trappiste@gmail.com>
2024-10-09 17:17:11 +0200

    Configure firefox

--- users/common/firefox.nix
+++ users/common/firefox.nix
@@ -3,7 +3,7 @@
 {
   programs.firefox = {
     enable = true;
-    #languagePacks = [ "es-ES" ];
+    languagePacks = [ "es-ES" "en-US" ];
 
     policies = {
       DisableTelemetry = true;
@@ -11,17 +11,38 @@
       #DontCheckDefaultBrowser = true;
       #DisablePocket = true;
       SearchBar = "separate";
+      OverrideFirstRunPage = "";
+
+      FirefoxHome = # Make new tab only show search
+      {
+        Search = true;
+        TopSites = false;
+        SponsoredTopSites = false;
+        Highlights = false;
+        Pocket = false;
+        SponsoredPocket = false;
+        Snippets = false;
+      };
+
+      Preferences = {
+        "widget.use-xdg-desktop-portal.file-picker" = 1; # Use new gtk file picker instead of legacy one
+      };
     };
 
     profiles = {
       default = {
         id = 0;
         name = "default";
+
         settings = {
           "browser.startup.homepage" = "https://nixos.org";
-          "browser.search.defaultenginename" = "DuckDuckGo";
-          "browser.search.order.1" = "DuckDuckGo";
+          "browser.search.region" = "ES";
+          "browser.search.isUS" = false;
+          "distribution.searchplugins.defaultLocale" = "es-ES";
+          "general.useragent.locale" = "es-ES";
+          "intl.locale.requested" = "es-ES,en-US";
         };
+
         search = {
           force = true;
           default = "DuckDuckGo";
